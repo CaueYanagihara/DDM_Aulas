0db8f24c68a4a6194e836a96d6a609b00d10a10a
CaueYanagihara <caueyanagihara@gmail.com>
2024-08-15 22:36:49 -0300

    Iniciado a implementação da validação de cpf.

---
+++ gestao_notas/android/app/src/main/kotlin/com/example/gestao_nota/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gestao_nota
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gestao_notas/dominio/cpf.dart
@@ -0,0 +1,55 @@
+import 'dart:ffi';
+import 'dart:math';
+
+class CPF {
+  late List<int> numerosCPF;
+  //LATE GARANTE QUE SERÁ DADO UM VALOR A VARIAVEL ATÉ O FIM DO CÓDIGO,
+  //PARA NÃO DAR ERRO DE VARIÁVEL NULA.
+
+  CPF(String cpf) {
+    if (cpf == '') throw Exception('CPF não pode ser vazio');
+
+    cpf = cpf.replaceAll(RegExp(r'\D'), '');
+    //TRANSFORMA TODOS OS CARACTERES QUE NÃO FOREM NUMERO EM ''.
+
+    numerosCPF = cpf.split('').map(int.parse).toList();
+    //PERCORRE CPF SEPARANDO OS DIGITOS E TRANSFORMANTO EM UMA LISTA DE INT
+  }
+
+  bool eOnzeNumeros() {
+    if (numerosCPF.length != 11)
+      throw Exception('CPF deve possuir 11 números!');
+    return true;
+  }
+
+  bool eNumerosDiferentes() {
+    return true;
+  }
+
+  bool eDigitosCorretos() {
+    return true;
+  }
+
+  bool eUnico() {
+    List<int> multNumeros = List<int>.filled(9, 0);
+
+    for (int i = 1; i <= 9; i++) {
+      int j = i - 1;
+      multNumeros[j] = numerosCPF[j] * i;
+    }
+
+    int soma = 0;
+    for (var valor in multNumeros) {
+      soma += valor;
+    }
+
+    int sobra = soma % 11;
+    for (int i = 0; i <= 10; i++) {
+      if (sobra == i) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
