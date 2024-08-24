bfea3adb1dacbf8f85ed763d8edfd03e9a21556a
Cauê Grande Yanagihara <134626371+CaueYanagihara@users.noreply.github.com>
2024-08-24 18:19:55 -0300

    Update

--- gestao_notas/dominio/cpf.dart
+++ gestao_notas/dominio/cpf.dart
@@ -1,61 +1,65 @@
-class CPF {
+class CPF{
   late List<int> numerosCPF;
-  //LATE GARANTE QUE SERÁ DADO UM VALOR A VARIAVEL ATÉ O FIM DO CÓDIGO,
-  //PARA NÃO DAR ERRO DE VARIÁVEL NULA.
 
-  CPF(String cpf) {
+
+  CPF(String cpf){
     eVazio(cpf);
-
     cpf = cpf.replaceAll(RegExp(r'\D'), '');
-    //TRANSFORMA TODOS OS CARACTERES QUE NÃO FOREM NUMERO EM ''.
-
     numerosCPF = cpf.split('').map(int.parse).toList();
-    //PERCORRE CPF SEPARANDO OS DIGITOS E TRANSFORMANTO EM UMA LISTA DE INT
-    
-    eOnzeNumeros();
-    eNumerosDiferentes();
-    eDigitosCorretos();
   }
 
-  bool eVazio(cpf) {
-    if (cpf == '') throw Exception('CPF não pode ser vazio');
+
+  eVazio(String cpf){
+    if(cpf.isEmpty) throw Exception('CPF não pode ser vazio!');
+  }
+
+
+  bool eOnzeNumeros(){
+    numerosCPF.forEach(print);
+    if(numerosCPF.length!=11)throw Exception('CPF precisa ter 11 números');
     return true;
   }
 
-  bool eOnzeNumeros() {
-    if (numerosCPF.length != 11)
-      throw Exception('CPF deve possuir 11 números!');
-    return true;
-  }
 
-  bool eNumerosDiferentes() {
-    return true;
-  }
-
-  bool eDigitosCorretos() {
-    return true;
-  }
-
-  bool eUnico() {
-    List<int> multNumeros = List<int>.filled(9, 0);
-
-    for (int i = 1; i <= 9; i++) {
-      int j = i - 1;
-      multNumeros[j] = numerosCPF[j] * i;
-    }
-
-    int soma = 0;
-    for (var valor in multNumeros) {
-      soma += valor;
-    }
-
-    int sobra = soma % 11;
-    for (int i = 0; i <= 10; i++) {
-      if (sobra == i) {
-        return true;
+  bool eNumeroDiferente(){
+    var diferente = false;
+    for(int i=0; i< 9; i++){
+      if(numerosCPF[i] != numerosCPF[i+1]) {
+        diferente = true;
+        break;
       }
     }
+    if(!diferente) throw Exception('CPF não pode ter números iguais');
+    return diferente;
+  }
 
-    return false;
+
+  bool eDigitoCorreto(){
+    verificarPrimeiroDigito();
+    verificarSegundoDigito();
+    return true;
+  }
+
+
+  int calcularDigito(int indice){
+    var soma=0;
+    for(var peso=9; indice >=0; peso--, indice--){
+      soma += numerosCPF[indice] * peso;
+    }
+    var digito = soma % 11;
+    if(digito==10) digito=0;
+    return digito;
+  }
+
+
+  bool verificarPrimeiroDigito(){
+    if(calcularDigito(8)!=numerosCPF[9]) throw Exception('Primeiro digito incorreto');
+    return true;
+  }
+
+
+  bool verificarSegundoDigito(){
+    if(calcularDigito(9)!=numerosCPF[10]) throw Exception('Segundo digito incorreto');
+    return true;
   }
 }
